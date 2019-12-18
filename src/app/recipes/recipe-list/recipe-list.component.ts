0bde05ac3b269c319888dc38ef9ee724f4607d93
Paul Waldo <paul.w.waldo@gmail.com>
2019-12-18 16:20:41 -0500

    Shows recipes in recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is just a test',
+      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is just a test',
+      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
