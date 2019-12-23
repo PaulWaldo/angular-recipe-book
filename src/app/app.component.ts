403f267e54bb84612604722bac8bf5d6c151dc7c
Paul Waldo <paul.w.waldo@gmail.com>
2019-12-23 07:53:21 -0500

    Allows menu to select between recipe and shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-recipe-book';
+  selectedComponent = 'recipe';
+
+  onNavigate(component: string) {
+    this.selectedComponent = component;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output() componentSelectedEvent = new EventEmitter<string>();
+
+  onComponentSelected(component: string) {
+    this.componentSelectedEvent.emit(component);
+  }
 }
