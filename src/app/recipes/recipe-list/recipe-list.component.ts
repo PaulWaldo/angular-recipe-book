cfd47d30208445c57272d091507321a081b7897f
Paul Waldo <paul.w.waldo@gmail.com>
2019-12-23 14:13:07 -0500

    Displays recipe details when a recipe is selected

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
@@ -14,8 +15,8 @@ export class RecipeListComponent implements OnInit {
       'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
     ),
     new Recipe(
-      'A Test Recipe',
-      'This is just a test',
+      'A Second Recipe',
+      'Yet Another test',
       'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
     )
   ];
@@ -23,4 +24,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
