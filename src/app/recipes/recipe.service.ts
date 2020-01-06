159052fa90bfbbb4e0be7974b7ba373ee69501ae
Paul Waldo <paul.w.waldo@gmail.com>
2020-01-06 16:29:29 -0500

    Creates Recipe Service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is just a test',
+      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
+    ),
+    new Recipe(
+      'A Second Recipe',
+      'Yet Another test',
+      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
+    )
+  ];
+
+  getRecipes() {
+    // Return a copy to prevent modification
+    return this.recipes.slice();
+  }
+
+  constructor() {}
+}
