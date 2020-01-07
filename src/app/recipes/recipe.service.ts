68b650ab579c171727d80a9908d627af7aa0701b
Paul Waldo <paul.w.waldo@gmail.com>
2020-01-07 06:20:16 -0500

    Shows ingredient list in recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,16 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is just a test',
-      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/2560px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
-      'A Second Recipe',
-      'Yet Another test',
-      'https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe.jpg'
+      'Big Fat Burger',
+      'Say no more!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/2560px-RedDot_Burger.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     )
   ];
 
